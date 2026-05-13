fb1160a5aab66c8601a3590c07ea452b7f8ffd3e
ealting7 <ealting911@gmail.com>
2026-05-13 18:47:49 -0400

    sidenav refactor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from './layout/sidebar/pages/sidebar';
+import { Header } from './layout/header/pages/header';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidebar, Header, MatSidenavModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+
   protected readonly title = signal('scripture-hub-client');
+  sidebarCollapsed = signal(false);
+
+  toggleSidebar() {
+    this.sidebarCollapsed.update(collapsed => !collapsed);
+  }
 }
