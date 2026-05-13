f5c4a548c353a127e29badbd11136c5ae13faf72
ealting7 <ealting911@gmail.com>
2026-05-13 18:47:11 -0400

    added inital routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./core/features/dashboard/pages/dashboard').then(m => m.Dashboard),
+        pathMatch: 'full'
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./core/features/dashboard/pages/dashboard').then(m => m.Dashboard),
+        pathMatch: 'full'
+    }
+];
