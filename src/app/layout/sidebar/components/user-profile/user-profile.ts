f848be2a4f0634e03b72f3de2c38b4ae15771c0a
ealting7 <ealting911@gmail.com>
2026-05-15 09:45:31 -0400

    refactored user profile page

--- src/app/layout/sidebar/components/user-profile/user-profile.ts
+++ src/app/layout/sidebar/components/user-profile/user-profile.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { LayoutService } from '../../../../core/services/layout.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector: 'app-user-profile',
-  imports: [],
+  selector: 'sidebar-user-profile',
+  imports: [MatButtonModule],
   templateUrl: './user-profile.html',
   styleUrl: './user-profile.scss',
 })
-export class UserProfile {}
+export class UserProfile {
+
+  private layoutService = inject(LayoutService);
+  isOpenProfile = input(true);
+
+  public handleSelectNaviationItem(event: Event, navigationItem: string) {
+    event.preventDefault();
+    this.layoutService.handleSelectNaviationItem(navigationItem);
+  }
+}
