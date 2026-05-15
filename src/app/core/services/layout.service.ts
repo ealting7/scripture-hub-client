e7b0be003e175ed28c83cd913b539e62ba5411c2
ealting7 <ealting911@gmail.com>
2026-05-15 09:40:51 -0400

    added navigation selected item; updated routing

--- src/app/core/services/layout.service.ts
+++ src/app/core/services/layout.service.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class LayoutService {
 
+    private router = inject(Router);
     private sidenav!: MatSidenav;
     private isOpen = false;
+
+    public selectedNavigationItem = signal('');
 
     public setSidenav(sidenav: MatSidenav) {
         this.sidenav = sidenav;
@@ -14,5 +18,48 @@ export class LayoutService {
     public toggle(): void {
         this.sidenav.toggle(this.isOpen);
         this.isOpen = !this.isOpen;
+    }
+
+    public handleSelectNaviationItem(navigationItem: string) {
+        this.setSelectedNavigationItem(navigationItem);
+        this.routeToSelectedItem(navigationItem);
+    }
+
+    private setSelectedNavigationItem(item: string): void {
+        this.selectedNavigationItem.set(item);
+    }
+
+    private routeToSelectedItem(item: string): void {
+        switch (item) {
+            case 'Dashboard':
+                this.router.navigate(['/dashboard']);
+                break;
+            case 'Bible Reader':
+                this.router.navigate(['/bible-reader']);
+                break;
+            case 'My Journal':
+                this.router.navigate(['/my-journal']);
+                break;
+            case 'Sermon Study':
+                this.router.navigate(['/sermon-study']);
+                break;
+            case 'Prayer Tracker':
+                this.router.navigate(['/prayer-tracker']);
+                break;
+            case 'My Groups':
+                this.router.navigate(['/my-groups']);
+                break;
+            case 'Giving Portal':
+                this.router.navigate(['/giving-portal']);
+                break;
+            case 'Marketplace':
+                this.router.navigate(['/marketplace']);
+                break;
+            case 'User Profile':
+                this.router.navigate(['/user-profile']);
+                break;
+            default:
+                break;
+        }
     }
 }
