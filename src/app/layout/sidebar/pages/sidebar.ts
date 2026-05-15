7a5f0ec8aa87a1a6b76a9c0b21af3692f788ab0b
ealting7 <ealting911@gmail.com>
2026-05-15 09:47:46 -0400

    added new sidebar components; added selected nav signal

--- src/app/layout/sidebar/pages/sidebar.ts
+++ src/app/layout/sidebar/pages/sidebar.ts
@@ -1,21 +1,25 @@
-import { Component, input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, input, ChangeDetectionStrategy, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { Dashboard } from '../components/dashboard/dashboard';
 import { Study } from '../components/study/study';
 import { Community } from '../components/community/community';
 import { Church } from '../components/church/church';
+import { UserProfile } from '../components/user-profile/user-profile';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [MatButtonModule, MatIconModule, CommonModule, Study, Community, Church],
+  imports: [MatButtonModule, MatIconModule, CommonModule, Dashboard, Study, Community, Church, UserProfile],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Sidebar {
+
   toggleSidebar = input<() => void>(() => { });
-  isOpen = input(true);
+  isOpenSidebar = input(true);
+  incomingSidebarNavigationItem = signal('');
 
   onToggle() {
     this.toggleSidebar()();
