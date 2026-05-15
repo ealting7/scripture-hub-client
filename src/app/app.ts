95e3c0093dd2e941bc98c22a9572fba0b66d152a
ealting7 <ealting911@gmail.com>
2026-05-15 09:38:19 -0400

    removed import item

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './layout/sidebar/pages/sidebar';
 import { Header } from './layout/header/pages/header';
