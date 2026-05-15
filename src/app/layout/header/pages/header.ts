d66676dca718462e1ae74350d14fc218147ba085
ealting7 <ealting911@gmail.com>
2026-05-15 09:42:41 -0400

    added selected navigation to header

--- src/app/layout/header/pages/header.ts
+++ src/app/layout/header/pages/header.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { Title } from '../components/title/title';
+import { SelectedNavigation } from '../components/selected-navigation/selected-navigation';
 
 @Component({
   selector: 'app-header',
-  imports: [Title],
+  imports: [Title, SelectedNavigation],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header { }
+export class Header {
+
+}
